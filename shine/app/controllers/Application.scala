aaa7a6bd1d6ed0a51b7e32ecf68f642365047a61
kinmanli <kinman.li@bl.uk>
2014-02-20 09:39:37 +0000

    Few more bits to the graphs and structuring the search templates

--- shine/app/controllers/Application.scala
+++ shine/app/controllers/Application.scala
@@ -7,6 +7,12 @@ import uk.bl.wa.shine.Shine
 import uk.bl.wa.shine.Query
 import uk.bl.wa.shine.Rescued
 import uk.bl.wa.shine.Pagination
+import uk.bl.wa.shine.GraphData
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import org.apache.commons.lang3.StringUtils
+import scala.collection.mutable.ListBuffer
 
 object Application extends Controller {
   
@@ -22,27 +28,17 @@ object Application extends Controller {
 		Ok(views.html.index("Shine Application"))
 	}
   
-	def graph = Action {
-		val data = List.fill(50)(scala.util.Random.nextInt)
-		Ok(views.html.graphs.plot("Plot Graph Test", "label x", "label y", data))
-	}
-	
 	def halflife = Action {
 	    rescued.halflife();
 	    Ok(views.html.index("Half-life..."))
 	}
   
 	def search(query: String, pageNo: Int, sort: String, order: String) = Action { implicit request =>
-	    val map = request.queryString
-	    val javaMap = map.map { case (k,v) => (k, v.asJava) }.asJava;
-	    val q = new Query()
-	    q.query = query
-	    q.parseParams(javaMap)
-	    q.res = solr.search(query, q.filters, pageNo, sort, order)
+
+		val q = doSearch(query, request.queryString, sort, order)
 	    
 	    val totalRecords = q.res.getResults().getNumFound().intValue()
 		val recordsPerPage = solr.getPerPage()
-		val currentPageSize = q.res.getResults().size()
 		
 		println("Page #: " + pageNo)
 	  	println("totalRecords #: " + totalRecords)
@@ -55,16 +51,11 @@ object Application extends Controller {
   
 	def advanced_search(query: String, pageNo: Int, sort: String, order: String) = Action { implicit request =>
 		println("advanced_search")
-		val map = request.queryString
-		val javaMap = map.map { case (k,v) => (k, v.asJava) }.asJava;
-		val q = new Query()
-		q.query = query
-		q.parseParams(javaMap)
-		q.res = solr.search(query, q.filters, pageNo, sort, order)
-  
+
+		val q = doSearch(query, request.queryString, sort, order)
+
 		val totalRecords = q.res.getResults().getNumFound().intValue()
 		val recordsPerPage = solr.getPerPage()
-		val currentPageSize = q.res.getResults().size()
 	
 		println("Page #: " + pageNo)
 		println("totalRecords #: " + totalRecords)
@@ -73,4 +64,66 @@ object Application extends Controller {
 		pagination.update(totalRecords, recordsPerPage, pageNo)
 	  	Ok(views.html.search.advanced(q, pagination, sort, order))
 	}
+	
+	def plot_graph(query: String, year_start: String, year_end: String) = Action { implicit request =>
+	
+		var yearStart = year_start
+		var yearEnd = year_end
+		
+		if (StringUtils.isBlank(yearStart)) {
+			yearStart = config.getString("default_from_year")
+		}
+		if (StringUtils.isBlank(yearEnd)) {
+			yearEnd = config.getString("default_end_year")
+		}
+		val q = doSearch(query, request.queryString, "", "")
+		val totalRecords = q.res.getResults().getNumFound().intValue()
+		println("totalRecords: " + totalRecords);
+
+	    val from_year: Int = yearStart.toInt
+	    val to_year: Int = yearEnd.toInt
+
+	    var data = new ListBuffer[GraphData]()
+
+	    var j = 1
+		for(i <- from_year.to(to_year).by(scala.math.pow(20,j).toInt)) {
+			// i = year
+			// j = data
+			j = j + 1
+			var graphData = new GraphData(i, j, query)
+			println(graphData.getYear + " " + graphData.getData + " " + graphData.getName)
+			data += graphData
+		}
+		println("plotting.... " + data + " " + query + " " + yearStart + " " + yearEnd)
+	    
+	    Ok(views.html.graphs.plot("Plot Graph Test", query, "Years", "label Y", data, yearStart, yearEnd))
+	}
+	
+	def doSearch(query: String, queryString: Map[String, Seq[String]], sort: String, order: String) = {
+		val map = queryString
+		val javaMap = map.map { case (k,v) => (k, v.asJava) }.asJava;
+		println("javaMap: " + javaMap)
+		val q = new Query()
+		q.query = query
+		q.parseParams(javaMap)
+		q.res = solr.search(query, q.filters, 0, sort, order)
+		q
+	}
+	
+	def sum(num: Integer) = {
+		var prevNum = num
+		prevNum = prevNum + 20 
+		val newNum = prevNum
+		newNum
+	}
+	
+	def currentYear() = {
+		val today = Calendar.getInstance().getTime()
+		val yearFormat = new SimpleDateFormat("yyyy")
+		val currentYear = yearFormat.format(today)
+		println("currentYear: " + currentYear)
+		currentYear
+	}
+
+	case class Mut[A](var value: A) {}
 }
