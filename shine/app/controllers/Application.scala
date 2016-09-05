98cc337038e1083f1bdf001776b4a66fdb9cbaeb
Niels Kristian Hansen Skovmand <nkhs@statsbiblioteket.dk>
2016-09-05 12:29:26 +0200

    Remove trait Secured

--- shine/app/controllers/Application.scala
+++ shine/app/controllers/Application.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import models._
-import play.api._
 import play.api.data.Forms._
 import play.api.data._
 import play.api.mvc._
@@ -108,30 +107,5 @@ object Application extends Controller {
         routes.javascript.Account.saveCorpus,
         routes.javascript.Account.saveResources
       )).as("text/javascript")
-  }
-}
-
-/**
-  * Provide security features
-  */
-trait Secured {
-
-  /**
-    * Retrieve the connected user email.
-    */
-  private def username(request: RequestHeader) = request.session.get("email")
-
-  /**
-    * Redirect to login if the user in not authorized.
-    */
-  private def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
-
-  // --
-
-  /**
-    * Action for authenticated users.
-    */
-  def IsAuthenticated(f: => String => Request[AnyContent] => Result) = Security.Authenticated(username, onUnauthorized) { user =>
-    Action(request => f(user)(request))
   }
 }
