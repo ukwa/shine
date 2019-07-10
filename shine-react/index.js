1514d436431385941538aa86830df6872b567781
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-07-10 10:29:25 +0100

    Add working text filter.

--- shine-react/index.js
+++ shine-react/index.js
@@ -17,7 +17,7 @@ import {
 
 // The search fields and filterable facets you want
 const fields = [
-	{label: "All text fields", field: "*", type: "text"},
+	{label: "All text fields", field: "text", type: "text"}, // Using field:text rather than field:* as this matches our setup.
 	{label: "Title", field: "title", type: "text"},
 	{label: "Host", field: "host", type: "list-facet"},
 	{label: "Year of capture", field: "crawl_year", type: "range-facet"},
--- shine-react/web.js
+++ shine-react/web.js
@@ -25,7 +25,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 
 // The search fields and filterable facets you want
-var fields = [{ label: "All text fields", field: "*", type: "text" }, { label: "Title", field: "title", type: "text" }, { label: "Host", field: "host", type: "list-facet" }, { label: "Year of capture", field: "crawl_year", type: "range-facet" }, { label: "Content type", field: "content_type_norm", type: "list-facet" }];
+var fields = [{ label: "All text fields", field: "text", type: "text" }, // Using field:text rather than field:* as this matches our setup.
+{ label: "Title", field: "title", type: "text" }, { label: "Host", field: "host", type: "list-facet" }, { label: "Year of capture", field: "crawl_year", type: "range-facet" }, { label: "Content type", field: "content_type_norm", type: "list-facet" }];
 
 // The sortable fields you want
 var sortFields = [{ label: "Title", field: "title" }, { label: "Date of crawl", field: "crawl_date" }, { label: "Relevance", field: "score" }];
